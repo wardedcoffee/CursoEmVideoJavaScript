6219734ec13aa3c7816679f70da179262273ec87
Marcos Nascimento <marcozzymandias@gmail.com>
2021-04-07 13:26:59 -0300

    exVamosContar e Tabuada concluidos

--- aula14ex/ex0016/script.js
+++ aula14ex/ex0016/script.js
@@ -1,3 +1,43 @@
-document.querySelector('#init')
-var init = Number.value;
-console.log(init)
+function ver() {
+    var inicio = ''
+    var inicio = window.document.querySelector('input#inicio')
+    var show1 = Number(inicio.value)
+    console.log(inicio.value)
+    
+    var fim = ''
+    var fim = window.document.querySelector('input#fim')
+    var show2 = Number(fim.value)
+    console.log(fim.value)
+
+    var passo = ''
+    var passo = window.document.querySelector('input#passo')
+    var show3 = Number(passo.value)
+    console.log(passo.value)
+    
+    var resultado = window.document.querySelector('p#showText')
+    // voce tem que criar uma logica ou usar algum commando pra fazer o pulo entre o show 1 e o show 2!
+    //var resultado2 = resultado.innerText = `${show1} + ${show2} + ${show3}`
+    var resultado3 = show1 + show3
+    console.log(resultado3)
+
+    
+        for (var c = show1; c < show2; c++) {
+            console.log(`numero de pulos ${c}`)
+        }
+        
+
+        var resultadoZero = resultado.innerText = `todos os pulos ${c}`
+    /*
+    while ( show1 <= show2 ) {
+        console.log(`numero de pulos ${show1}`)
+        show1 ++
+    }
+    
+     
+tem que pegar o valor de inicio, 
+pegar o valor de fim, criar um intervalo,
+baseado no valor de passo, 
+que tem que começar com um(mais uma verificação)
+*/ 
+
+}
--- aula14ex/ex0017/script.js
+++ aula14ex/ex0017/script.js
@@ -1,1 +1,55 @@
-alert('olá')
+function ver() {
+    var inicio = ''
+    var inicio = window.document.querySelector('input#inicio')
+    var show1 = Number(inicio.value)
+    console.log(inicio.value)
+    
+    var tab = x = show1
+
+
+    var resultado = window.document.querySelector('fieldset#campoTxt')
+    console.log(resultado)
+
+    
+    var v1 = x * 1
+    var v2 = x * 2
+    var v3 = x * 3
+    var v4 = x * 4
+    var v5 = x * 5
+    var v6 = x * 6
+    var v7 = x * 7
+    var v8 = x * 8
+    var v9 = x * 9
+    var v10 = x * 10
+
+    //console.log(`${v1}`)
+
+
+    var resultadoZero = resultado.innerText = `Tabuada do ${x}
+    ${x} x 1 = ${v1}
+    ${x} x 2 = ${v2}
+    ${x} x 3 = ${v3}
+    ${x} x 4 = ${v4} 
+    ${x} x 5 = ${v5}
+    ${x} x 6 = ${v6}
+    ${x} x 7 = ${v7}
+    ${x} x 8 = ${v8}
+    ${x} x 9 = ${v9}
+    ${x} x 10 = ${v10}
+    
+    `
+
+
+}
+
+
+                    // x * 1 = 
+                    // x * 2 = 
+                    // x * 3 = 
+                    // x * 4 = 
+                    // x * 5 = 
+                    // x * 6 = 
+                    // x * 7 = 
+                    // x * 8 = 
+                    // x * 9 = 
+                    // x * 10 = 
