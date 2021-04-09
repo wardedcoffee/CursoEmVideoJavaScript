5d896212490fec21adfa5c871ab74cc9829acea5
Marcos Nascimento <marcozzymandias@gmail.com>
2021-04-08 22:53:49 -0300

    fim da aula15 variaveis compostas

---
+++ aula15/ambiente.js
@@ -0,0 +1,13 @@
+let num = [5, 8, 9, 2, 3]
+
+num.sort()
+num.push(1)
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+let pos = num.indexOf(8)
+if(pos == -1){
+    console.log(`Não foi possível encontrar o valor pesquisado`)
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
